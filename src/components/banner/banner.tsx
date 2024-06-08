0737f5192b35c0d166154ce5b3adf6e24ba83bff
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-06-09 03:42:58 +0700

    feat: add animation

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,20 +1,20 @@
 export default function Banner() {
   return (
-    <div className="relative w-screen h-[100vh] overflow-hidden ">
+    <div className="relative w-screen h-[100vh] overflow-hidden">
       <video autoPlay muted loop className="w-full h-[90%] object-cover">
         <source src="/videos/banner.mp4" type="video/mp4" />
         Her feeling banner video
       </video>
-      <div className="absolute inset-0 flex justify-start mt-24 flex-col text-center">
-        <div className="p-4 text-8xl font-semibold text-white space-y-10 text-center">
-          <div>Understand Her Emotions</div>
-          <div>Make Her Pround</div>
+      <div className="absolute inset-0 flex justify-start mt-36 flex-col text-center">
+        <div className="p-4 text-8xl text-white space-y-10 text-center">
+          <div className="move-up">Understand Her Emotions</div>
+          <div className="move-up-delay-1">Make Her Proud</div>
         </div>
-        <div className="text-white">
+        <div className="text-white move-up-delay-2">
           <div>
             By text classifier model, We can predict her feeling by text
           </div>
-          <div>let me help you.</div>
+          <div>Let me help you.</div>
         </div>
       </div>
     </div>
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <nav className="fixed top-0 w-screen h-16 flex flex-row px-8 items-center z-50">
-      <div className="text-white text-center items-center font-bold flex flex-row">
+      <div className="text-white text-center items-center font-bold flex flex-row fade-in">
         <Image
           alt="her feeling"
           src="/images/logo/logo_white.png"
