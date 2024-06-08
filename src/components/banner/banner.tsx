c3ed332e66deb605a954c40cfc6e438dfdac8a78
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-06-08 23:39:05 +0700

    feat/video banner

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -2,7 +2,7 @@ export default function Banner() {
   return (
     <div className="w-screen h-[90vh] overflow-hidden">
       <video autoPlay muted loop className="w-full h-full object-cover">
-        <source src="/videos/flower_white_banner.mp4" type="video/mp4" />
+        <source src="/videos/banner_3.mp4" type="video/mp4" />
         Her feeling banner video
       </video>
     </div>
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 w-screen h-16 flex flex-row px-8 items-center">
+      <div className="text-white text-center items-center font-bold">LOGO</div>
+    </nav>
+  );
+}
