18c505785618aef37e67133c4814472b9933ac43
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-06-09 00:07:50 +0700

    feat: add font

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,10 +1,22 @@
 export default function Banner() {
   return (
-    <div className="w-screen h-[90vh] overflow-hidden">
+    <div className="relative w-screen h-[90vh] overflow-hidden ">
       <video autoPlay muted loop className="w-full h-full object-cover">
         <source src="/videos/banner_3.mp4" type="video/mp4" />
         Her feeling banner video
       </video>
+      <div className="absolute inset-0 flex justify-center flex-col text-center">
+        <div className="p-4 text-8xl font-bold text-white space-y-10 text-center">
+          <div>Understand Her Emotions</div>
+          <div>Make Her Pround</div>
+        </div>
+        <div className="text-white">
+          <div>
+            By text classifier model, We can predict her feeling by text
+          </div>
+          <div>let me help you.</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 w-screen h-16 flex flex-row px-8 items-center">
+    <nav className="fixed top-0 w-screen h-16 flex flex-row px-8 items-center z-50">
       <div className="text-white text-center items-center font-bold">LOGO</div>
     </nav>
   );
