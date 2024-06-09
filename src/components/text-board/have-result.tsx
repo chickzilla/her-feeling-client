82d5282f02ff8c3c05b5bcb233402c393344e975
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-06-09 21:26:26 +0700

    feat: resize emoji

--- src/components/text-board/have-result.tsx
+++ src/components/text-board/have-result.tsx
@@ -1,37 +1,44 @@
-import { Frown } from "lucide-react";
+import {
+  BsEmojiLaughing,
+  BsEmojiTear,
+  BsEmojiKiss,
+  BsEmojiAngry,
+  BsEmojiAstonished,
+  BsEmojiDizzy,
+} from "react-icons/bs";
 import ResultItem from "./result-item";
 
 export default function HaveResult() {
   return (
     <div className="flex flex-col items-center justify-center text-center text-gray-400 space-y-5">
       <div className="move-right-to-left-0">
-        <ResultItem mood="Sadness" percent={0.85}>
-          <Frown size={100} style={{ color: "GrayText" }} />
+        <ResultItem mood="Sadness" percent={0.85} color="gray">
+          <BsEmojiTear size={50} style={{ color: "black" }} />
         </ResultItem>
       </div>
       <div className="move-right-to-left-1">
-        <ResultItem mood="Sadness" percent={0.51}>
-          <Frown size={100} style={{ color: "GrayText" }} />
+        <ResultItem mood="Joy" percent={0.51} color="yellowgreen">
+          <BsEmojiLaughing size={50} style={{ color: "black" }} />
         </ResultItem>
       </div>
       <div className="move-right-to-left-2">
-        <ResultItem mood="Sadness" percent={0.0012}>
-          <Frown size={100} style={{ color: "GrayText" }} />
+        <ResultItem mood="Love" percent={0.5} color="pink">
+          <BsEmojiKiss size={50} style={{ color: "black" }} />
         </ResultItem>
       </div>
       <div className="move-right-to-left-3">
-        <ResultItem mood="Sadness" percent={0.0012}>
-          <Frown size={100} style={{ color: "GrayText" }} />
+        <ResultItem mood="Angry" percent={0.5} color="red">
+          <BsEmojiAngry size={50} style={{ color: "black" }} />
         </ResultItem>
       </div>
       <div className="move-right-to-left-4">
-        <ResultItem mood="Sadness" percent={0.0012}>
-          <Frown size={100} style={{ color: "GrayText" }} />
+        <ResultItem mood="Fear" percent={0.5} color="purple">
+          <BsEmojiDizzy size={50} style={{ color: "black" }} />
         </ResultItem>
       </div>
       <div className="move-right-to-left-5">
-        <ResultItem mood="Sadness" percent={0.0012}>
-          <Frown size={100} style={{ color: "GrayText" }} />
+        <ResultItem mood="Surprise" percent={0.5} color="orange">
+          <BsEmojiAstonished size={50} style={{ color: "black" }} />
         </ResultItem>
       </div>
     </div>
--- src/components/text-board/result-item.tsx
+++ src/components/text-board/result-item.tsx
@@ -13,18 +13,35 @@ export default function ResultItem({
   mood,
   percent,
   children,
+  color,
 }: {
   mood: string;
   percent: number;
   children: React.ReactNode;
+  color: string;
 }) {
   return (
-    <div className="flex items-center justify-start text-center text-gray-400  space-x-5 p-2 move-right-to-left">
+    <div className="flex items-center justify-start text-center text-gray-400  space-x-8 px-2 move-right-to-left">
       <div>{children}</div>
-      <div className="flex flex-col space-y-2 bg-white rounded-full text-black py-5 px-10 w-[40vw] items-start border-slate-300 border-2">
-        <div className="font-semibold text-lg">{mood}</div>
-        <div className="w-[80%]">
-          <BorderLinearProgress variant="determinate" value={percent * 100} />
+      <div className="flex flex-col space-y-2 bg-white rounded-full text-black py-5 px-10 w-[35vw] items-start border-slate-300 border-2">
+        <div className="flex flex-row space-x-7 items-center justify-center">
+          <div className="font-semibold text-2xl">{mood}</div>
+          <div className="text-center text-sm text-gray-500">
+            {(percent * 100).toFixed(2)}
+            {" %"}
+          </div>
+        </div>
+        <div className="w-[100%]">
+          <BorderLinearProgress
+            variant="determinate"
+            value={percent * 100}
+            sx={{
+              backgroundColor: "whitesmoke",
+              "& .MuiLinearProgress-bar": {
+                backgroundColor: color,
+              },
+            }}
+          />
         </div>
       </div>
     </div>
