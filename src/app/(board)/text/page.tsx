b087fb1076ad94ae71b652736ff675b247cc55e3
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-06-10 01:02:16 +0700

    fix: fix

--- src/app/(board)/text/page.tsx
+++ src/app/(board)/text/page.tsx
@@ -1,15 +1,21 @@
+import HelperPanel from "@/components/text-board/helper-panel";
 import ResultPane from "@/components/text-board/resultPane";
 import TextAreaBoard from "@/components/text-board/text-area-board";
 
 export default function BoardTextPage() {
   return (
     <>
-      <main className="w-full pl-20 pr-5 pt-[100px] space-y-12 min-h-[100vh] overflow-y-hidden text-black bg-slate-100">
-        <div className="">
-          <TextAreaBoard />
-        </div>
-        <div>
-          <ResultPane />
+      <main className="w-full pl-20 pr-5 pt-[100px] space-y-12 min-h-[100vh] overflow-y-hidden text-black bg-slate-100 ">
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-col space-y-10">
+            <div className="">
+              <TextAreaBoard />
+            </div>
+            <div>
+              <ResultPane />
+            </div>
+          </div>
+          <HelperPanel />
         </div>
       </main>
     </>
