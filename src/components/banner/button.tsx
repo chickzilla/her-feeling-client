b194812a471514ac4ba61ad57f810fb5424b8663
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-06-14 00:39:53 +0700

    button banner

--- src/components/banner/button.tsx
+++ src/components/banner/button.tsx
@@ -1,18 +1,21 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { ArrowRight } from "lucide-react";
+
 export default function Button() {
   const router = useRouter();
   return (
     <button
-      className={`bg-white text-black py-3 px-5 rounded-full font-normal
+      className={`bg-white text-black py-3 px-5 rounded-full font-semibold
          min-w-40 hover:cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 
-            hover:bg-black hover:text-white hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110
+            hover:bg-black hover:text-white hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 flex text-center justify-center space-x-4 items-center text-lg
           `}
       onClick={() => {
         router.push("/text");
       }}
     >
-      Get start
+      <span>Get start</span>
+      <ArrowRight />
     </button>
   );
 }
