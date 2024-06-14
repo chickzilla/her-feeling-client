70fff5ad1d02f675cba31339b8c514c256f64bf9
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-06-14 23:13:06 +0700

    change  nav bar to transparent

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,7 @@ const config = {
         xl: "1.25rem", // Extra large screens
       },
       colors: {
+        pink: "bg-pink-500",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
