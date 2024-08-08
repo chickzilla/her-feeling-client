97930e7481a2a6a20cf04f1119778cc5b52b8cc3
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2024-08-08 23:24:10 +0700

    add metaData to layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,6 +1,13 @@
 import { Toaster } from "@/components/alert/toaster";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Navbar from "@/components/navbar/navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Login to HERFEEL - Text-Based Mood Prediction",
+	description:
+		"With HERFEEL, reveal the emotional insights concealed in words. You may improve your understanding of and relationships with the significant someone in your life by using our sophisticated mood prediction tool, which analyzes text to uncover underlying emotions. HERFEEL offers precise and customized mood forecasts to help you navigate relationships and strengthen bonds, whether you're just inquisitive or navigating a friendship.",
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
